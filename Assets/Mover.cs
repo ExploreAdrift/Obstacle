e5676f4783c4f9882028defbd37b4156418dad77
Nicholas Smith <103008849+ExploreAdrift@users.noreply.github.com>
2022-08-15 09:16:55 -0700

    Add files via upload

---
+++ Assets/Mover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // SerializeField connects VS Code to Unity
+    // Start is called before the first frame update
+
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player");
+        Debug.Log("Dodge everything");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        // STRING REFERENCE
+        transform.Translate(xValue, 0, zValue);
+    }
+
+
+}
